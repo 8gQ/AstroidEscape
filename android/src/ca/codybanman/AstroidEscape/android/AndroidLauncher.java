4dc6c2cc23252c3db54c687c629c1180a3f87cff
crbanman <crbanman@gmail.com>
2014-06-19 17:03:28 -0700

    Implementing AdMob Ads

--- android/src/ca/codybanman/AstroidEscape/android/AndroidLauncher.java
+++ android/src/ca/codybanman/AstroidEscape/android/AndroidLauncher.java
@@ -1,19 +1,55 @@
 package ca.codybanman.AstroidEscape.android;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
 import ca.codybanman.AstroidEscape.AEGame;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 
 public class AndroidLauncher extends AndroidApplication {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
-        cfg.useGLSurfaceView20API18 = true;
-        
-        initialize(new AEGame(), cfg);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+		cfg.useGLSurfaceView20API18 = true;
+
+		RelativeLayout layout = new RelativeLayout(this);
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getWindow().clearFlags(
+				WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+
+		View gameView = initializeForView(new AEGame(), cfg);
+
+		AdView adView = new AdView(this);
+		adView.setAdSize(AdSize.SMART_BANNER);
+		adView.setAdUnitId("ca-app-pub-7512999237077002/4617342779");
+		adView.loadAd(new AdRequest.Builder()
+				.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+				.addTestDevice("D4514AFD171EE7ABDD56A1FEA2B2D9AF").build());
+
+		
+
+		RelativeLayout.LayoutParams adParams = new RelativeLayout.LayoutParams(
+				RelativeLayout.LayoutParams.WRAP_CONTENT,
+				RelativeLayout.LayoutParams.WRAP_CONTENT);
+		adParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+		adParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+
+		layout.addView(adView, adParams);
+		layout.addView(gameView);
+
+		setContentView(layout);
+
+	}
 }
